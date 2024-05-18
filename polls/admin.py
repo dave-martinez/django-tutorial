277fb64652b64a9b506954c61d57889ce4a50c0d
Dave Martinez <hi@davemartinez.dev>
2024-05-18 22:40:25 +1000

    Add admin and css

--- polls/admin.py
+++ polls/admin.py
@@ -4,5 +4,22 @@ from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date']})
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
